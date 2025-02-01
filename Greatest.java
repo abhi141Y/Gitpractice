20498cef53e7a9bddcd7892600eaebd0dc6f5b28
Nanneboina abhinav <nanneboinaabhinav@Nanneboinas-MacBook-Air.local>
2025-02-01 12:41:41 +0530

    greatest of two numbers

---
+++ Greatest.java
@@ -0,0 +1,12 @@
+public class Demo2 {
+    public static void main(String[] args){
+        int num1=7;
+        int num2=10;
+        if (num1>num2) {
+            System.out.println("num1 is less than num2");
+        }
+        else{
+            System.out.println("num2 is greater");
+        }
+    }
+}
